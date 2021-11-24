faf59a771529886bce50bf60a444765c49146972
jkl46 <jay-klein@outlook.com>
2021-11-24 10:21:41 +0100

    cart update

--- cart.php
+++ cart.php
@@ -11,15 +11,15 @@ $cart = getCart();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    <div style="text-align:center;width: 600px;margin: 0 auto;"> 
-<h1>Inhoud Winkelwagen</h1>
-<table>
+<div class='main-content'> 
+<h1 class='cart-title'>Inhoud Winkelwagen</h1>
+<table class='cart-table'>
   <tr>
   <th id="col-image"></th>
   <th>Naam</th>
-  <th class="add"></th>
-  <th>Aantal</th>
-  <th class="remove"></th>
+  <th class="add-col"></th>
+  <th class='amount-col'>Aantal</th>
+  <th class="remove-col"></th>
   <th>Per stuk</th>
   <th>Totaal</th>
 </tr>
@@ -32,22 +32,19 @@ $cart = getCart();
     style="background-image: url('Public/StockItemIMG/<?php print $StockItemImage[0]['ImagePath']; ?>'); width: 50px; height: 50px; background-size: 50px; background-repeat: no-repeat; background-position: center;"></th> 
     <?php
     print("<th>".$StockItem["StockItemName"]."</th>");
-    print("<th><a href='#' class='add-btn' label='$productID'><i class='material-icons add'>add</i></a></th>");
-    print("<th>".$count."</th>");
-    print("<th><a href='#' class='add-btn'><i class='material-icons remove'>remove</i></a></th>");
+    print("<th><a href='' class='add-btn' label='$productID'><i class='material-icons add'>add</i></a></th>");
+    print("<th><input class='amount-input' type='text' name='productCount' placeholder='...' value='$count'></th>");
+    print("<th><a href='#' class='remove-btn'><i class='material-icons remove'>remove</i></a></th>");
     print("<th>".number_format($StockItem['SellPrice'], 2)."</th>");
-    print("<th>".number_format($amount*$StockItem['SellPrice'], 2)."</th>");
+    print("<th>".number_format($count*$StockItem['SellPrice'], 2)."</th>");
     print("</tr>");
   }
   ?>
 </table> 
-<p><a href='/'>Terug naar webshop</a></p>
 </div>
 <script>
 $('.add-btn').click((e) => {
-  console.log(e.attributs['label']);
-  console.log(e);
-
+ console.log(e);
 }
 )
 
